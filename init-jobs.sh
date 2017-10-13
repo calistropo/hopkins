e7f4fa18a8469720f316167e3f0738957203060d
caeus <camilo.a.navas@gmail.com>
2017-10-13 09:47:46 +0200

    barracuda -> calistropo

--- init-jobs.sh
+++ init-jobs.sh
@@ -3,6 +3,6 @@
 export JOBS_DEPS=com.typesafe.play:play-json_2.11:2.6.6,org.apache.spark:spark-streaming-kafka-0-10_2.11:2.2.0,datastax:spark-cassandra-connector:2.0.1-s_2.11
 for var in "$@"
 do
-  /spark/bin/spark-submit --master local[4] --packages $JOBS_DEPS --class "com.auditest.Main" jobs.jar $var
+  /spark/bin/spark-submit --master local[4] --packages $JOBS_DEPS --class "com.calistropo.Main" jobs.jar $var
 done
 
--- src/main/scala/com/barracuda/tiles/TileStreams.scala
+++
@@ -1,8 +0,0 @@
-package com.barracuda.tiles
-
-object TileStreams {
-
-
-
-
-}
